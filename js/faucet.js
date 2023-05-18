4e8160797f5a855e306e8ba341a4af130343d341
KJ <kernel1983@gmail.com>
2023-05-18 10:25:37 +0800

    show account on switch

--- js/faucet.js
+++ js/faucet.js
@@ -23,76 +23,91 @@ if(window.location.host == 'bitpow.org'){
 }
 
 
-window.addEventListener('load', async () => {
+window.onload = async () => {
     let provider = null;
     let signer = null;
-    // const eth_accounts = document.getElementById('eth_accounts');
+
     const metamask_connect_btn = document.getElementById('metamask_connect');
+    const eth_account = document.getElementById('eth_account');
     const add_contract = document.getElementById('add_contract');
     const get_u = document.getElementById('get_u');
 
-    if (typeof window.ethereum !== 'undefined') {
-        console.log('MetaMask is installed!');
-        
-        provider = new ethers.providers.Web3Provider(window.ethereum);
-        signer = provider.getSigner();
+    if (typeof window.ethereum === 'undefined') {
+        console.log('MetaMask is not installed!');
+        return
+    }
 
-        // metamask_connect_btn.style.display = 'block';
-        metamask_connect_btn.addEventListener('click', async () => {
-            if(window.location.host == 'bitpow.org'){
-                await ethereum.request({
-                    method: 'wallet_addEthereumChain',
-                    params: [{
-                        chainId: CHAIN_ID,
-                        chainName: CHAIN_NAME,
-                        nativeCurrency: {
-                            name: TOKEN_NAME,
-                            symbol: TOKEN_SYMBOL, // 2-6 characters long
-                            decimals: 18
-                        },
-                        rpcUrls: [RPC_URL],
-                        // blockExplorerUrls: 'https://bitfile.org/scan'
-                        //iconUrls?: string[]; // Currently ignored.
-                    }]
-                });
-            }
+    provider = new ethers.providers.Web3Provider(window.ethereum);
+    signer = provider.getSigner();
 
+    // metamask_connect_btn.style.display = 'block';
+    metamask_connect_btn.onclick = async () => {
+        if(window.location.host == 'bitpow.org'){
             await ethereum.request({
-                method: 'wallet_switchEthereumChain',
-                params: [{ chainId: CHAIN_ID }],
+                method: 'wallet_addEthereumChain',
+                params: [{
+                    chainId: CHAIN_ID,
+                    chainName: CHAIN_NAME,
+                    nativeCurrency: {
+                        name: TOKEN_NAME,
+                        symbol: TOKEN_SYMBOL, // 2-6 characters long
+                        decimals: 18
+                    },
+                    rpcUrls: [RPC_URL],
+                    // blockExplorerUrls: 'https://bitfile.org/scan'
+                    //iconUrls?: string[]; // Currently ignored.
+                }]
             });
+        }
 
-            const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-            // const eth_address = accounts[0];
-            // add_contract.style.display = 'block';
-
-            // for(let i in accounts) {
-            //     console.log(accounts[i]);
-            //     let div = document.createElement('div');
-            //     div.innerHTML = `${accounts[i]} <input type='button' value='领取测试U' class='add_contract'/>
-            //         <input type="button" value="领取测试U" class="get_u"/>`;
-            //     div.id = 'eth_'+accounts[i];
-            //     div.className = 'eth_account';
-            //     eth_accounts.append(div);
-            // }
-
-            // window.ethereum.on('accountsChanged', (accounts) => {
-            //     while (eth_accounts.firstChild) {
-            //         eth_accounts.removeChild(eth_accounts.firstChild);
-            //     }
-            //     eth_address = accounts[0];
-            //     console.log(eth_address);
-            //     for(let i in accounts){
-            //         console.log(accounts[i]);
-            //         const div = document.createElement('div');
-            //         div.innerHTML = `${accounts[i]}`;
-            //         div.id = `eth_${accounts[i]}`;
-            //         div.className = 'eth_account';
-            //         eth_accounts.append(div);
-            //     }
-            // });
+        await ethereum.request({
+            method: 'wallet_switchEthereumChain',
+            params: [{ chainId: CHAIN_ID }],
         });
+
+        const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+        console.log(accounts);
+
+        if (ethereum.selectedAddress) {
+            eth_account.innerText = ethereum.selectedAddress;
+        }
     };
+
+    if (ethereum.selectedAddress) {
+        eth_account.innerText = ethereum.selectedAddress;
+    }
+    // add_contract.style.display = 'block';
+
+    // for(let i in accounts) {
+    //     console.log(accounts[i]);
+    //     let div = document.createElement('div');
+    //     div.innerHTML = `${accounts[i]} <input type='button' value='领取测试U' class='add_contract'/>
+    //         <input type="button" value="领取测试U" class="get_u"/>`;
+    //     div.id = 'eth_'+accounts[i];
+    //     div.className = 'eth_account';
+    //     eth_accounts.append(div);
+    // }
+
+    window.ethereum.on('accountsChanged', (accounts) => {
+        // while (eth_accounts.firstChild) {
+        //     eth_accounts.removeChild(eth_accounts.firstChild);
+        // }
+        // eth_address = accounts[0];
+        // console.log(eth_address);
+        // for(let i in accounts){
+        //     console.log(accounts[i]);
+        //     const div = document.createElement('div');
+        //     div.innerHTML = `${accounts[i]}`;
+        //     div.id = `eth_${accounts[i]}`;
+        //     div.className = 'eth_account';
+        //     eth_accounts.append(div);
+        // }
+        if (ethereum.selectedAddress) {
+            eth_account.innerText = ethereum.selectedAddress;
+        }else{
+            eth_account.innerText = 'Not connected';
+        }
+    });
 
     // let provider2;
     // if(window.location.host == 'bitpow.org'){
@@ -133,4 +148,4 @@ window.addEventListener('load', async () => {
         alert(`U sent to ${to_address}`);
     };
 
-});
+};
